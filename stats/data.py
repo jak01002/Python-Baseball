902db829ff1a69bfed251ecdf19789223e8af7c9
jak01002 <jake_osgood@tjx.com>
2021-07-07 19:24:36 -0400

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -2,7 +2,7 @@ import os
 import glob
 import pandas as pd
 
-game_files = glob.glob(os.path.join(os.getcwd, 'games', '*.EVE'))
+game_files = glob.glob(os.path.join(os.getcwd,'games', '*.EVE'))
 
 game_files.sort()
 game_frames = []
@@ -12,7 +12,20 @@ for game_file in game_files:
     game_frames.append(game_frame)
 
 games = pd.concat(game_frames)
-games.loc['??', ['multi5']] = ''
+games.loc[games['multi5'] == '??', ['multi5']] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+
+identifiers = identifiers.fillna(method='ffill')
+
+identifiers.columns = ['game_id', 'year']
+
+games = pd.concat([games, identifiers], axis=1, sort=False)
+
+games = games.fillna(' ')
+
+games.loc[:, ['type']] = pd.Categorical(games.loc[:, ['type']])
+
+print(games.head())
+
 
